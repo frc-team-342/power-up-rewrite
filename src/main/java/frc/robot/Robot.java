413275e7089181d8640ce74b77bf14ee9bdc7bfe
connellsensational <connellsensational@gmail.com>
2019-12-28 17:17:40 -0500

    Removed commands & subsystems from Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,8 +11,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-import frc.robot.commands.drive.DriveWithJoystick;
-import frc.robot.subsystems.DriveSystem;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -24,8 +23,6 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-  private DriveSystem m_DriveSystem;
-  private DriveWithJoystick m_DriveWithJoystick;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -36,8 +33,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    m_DriveSystem = new DriveSystem();
-    m_DriveWithJoystick = new DriveWithJoystick(m_DriveSystem);
+
   }
 
   /**
@@ -98,8 +94,6 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-
-    CommandScheduler.getInstance().schedule(m_DriveWithJoystick);
   }
 
   /**
