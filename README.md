17aeaa746f1e8c5c64ae41a416cd3595f0f42b45
connellsensational <connellsensational@gmail.com>
2019-12-31 16:07:25 -0500

    Updated to WPILib 2020

--- README.md
+++
@@ -1,3 +0,0 @@
-# Power Up Rewrite
-
-Rewrite of the code for our robot from the 2018 season using features from WPILib 2020.
--- src/main/java/frc/robot/subsystems/LightSystem.java
+++ src/main/java/frc/robot/subsystems/LightSystem.java
@@ -7,7 +7,7 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilib2.SubsystemBase;
 
 import com.ctre.phoenix.CANifier;
 import com.ctre.phoenix.CANifier.LEDChannel;
